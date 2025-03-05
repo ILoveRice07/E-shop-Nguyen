48d004c1af4184144bb6f617a3e9081da70c00b5
ILoveRice07 <159123610+ILoveRice07@users.noreply.github.com>
2025-03-05 20:21:55 +0100

    HTML,CSS,JS_done

--- script.js
+++ script.js
@@ -1,1 +1,64 @@
+const btn = document.getElementById('btn-buy');
+btn.addEventListener('click', function(){
+    alert("Sorry! We are out of stock.")
+})
 
+const btn1 = document.getElementById('btn-buy1');
+btn1.addEventListener('click', function () {
+    alert("Sorry! We are out of stock.")
+})
+
+const btn2 = document.getElementById('btn-buy2');
+btn2.addEventListener('click', function () {
+    alert("Sorry! We are out of stock.")
+})
+
+const btn3 = document.getElementById('btn-buy3');
+btn3.addEventListener('click', function () {
+    alert("Sorry! We are out of stock.")
+})
+
+const btn4 = document.getElementById('btn-buy4');
+btn4.addEventListener('click', function () {
+    alert("Sorry! We are out of stock.")
+})
+
+const btn5 = document.getElementById('btn-buy5');
+btn5.addEventListener('click', function () {
+    alert("Sorry! We are out of stock.")
+})
+
+const btn6 = document.getElementById('btn-buy6');
+btn6.addEventListener('click', function () {
+    alert("Sorry! We are out of stock.")
+})
+
+const btn7 = document.getElementById('btn-buy7');
+btn7.addEventListener('click', function () {
+    alert("Sorry! We are out of stock.")
+})
+
+const btn8 = document.getElementById('btn-buy8');
+btn8.addEventListener('click', function () {
+    alert("Sorry! We are out of stock.")
+})
+
+const btn9 = document.getElementById('btn-buy9');
+btn9.addEventListener('click', function () {
+    alert("Sorry! We are out of stock.")
+})
+
+const btn10 = document.getElementById('btn-buy10');
+btn10.addEventListener('click', function () {
+    alert("Sorry! We are out of stock.")
+})
+
+const btn11 = document.getElementById('btn-buy11');
+btn11.addEventListener('click', function () {
+    alert("Sorry! We are out of stock.")
+})
+
+const btn12 = document.getElementById('btn-buy12');
+btn12.addEventListener('click', function () {
+    alert("Sorry! We are out of stock.")
+})
